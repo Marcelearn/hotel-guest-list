3580b2f84800c2f67fd06ae2591d6651dfeb2699
Marcelearn <jaworskimarcel@gmail.com>
2022-10-20 09:24:47 +0200

    Reverse component

---
+++ src/components/Reverse.js
@@ -0,0 +1,4 @@
+const Reverse = (str) => {
+    return str.split("-").reverse().join(".");
+};
+export default Reverse;
--- src/components/Room.js
+++ src/components/Room.js
@@ -1,15 +1,13 @@
-function Room({item}){
-    const reverse = (str) => {
-        return str.split("-").reverse().join(".");
-    };
+import Reverse from "./Reverse";
+function Room({item}){ 
     return (
         <div className="room">
             <h2 className="room__Nummer" value="roomNo">Room number: {item.roomNo}</h2>
             <h3>Guest</h3>
             <p className="room__guest" value="guestFirstname">Name: {item.guest.firstName}</p>
             <p className="room__guest" value="guestLastname">Lastname: {item.guest.lastName}</p>
-            <p className="room__guest__checkin" value="checkIn">checkIn: {reverse(item.checkIn)}</p>
-            <p className="room__guest__checkout" value="checkOut">checkOut: {reverse(item.checkOut)}</p>
+            <p className="room__guest__checkin" value="checkIn">checkIn: {Reverse(item.checkIn)}</p>
+            <p className="room__guest__checkout" value="checkOut">checkOut: {Reverse(item.checkOut)}</p>
         </div>
 
     );
